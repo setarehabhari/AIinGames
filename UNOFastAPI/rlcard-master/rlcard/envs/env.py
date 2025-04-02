fc9877f0c1c5c6877da2e70b698f46652428ab9a
marmary <maryam.kermanshahani6079@gmail.com>
2025-04-02 15:54:56 -0600

    Library changed

--- UNOFastAPI/rlcard-master/rlcard/envs/env.py
+++ UNOFastAPI/rlcard-master/rlcard/envs/env.py
@@ -117,6 +117,57 @@ class Env(object):
         '''
         self.agents = agents
 
+    def run_with_api(self, action):
+        '''
+        Run a complete game, either for evaluation or training RL agent.
+
+        Args:
+            is_training (boolean): True if for training purpose.
+
+        Returns:
+            (tuple) Tuple containing:
+
+                (list): A list of trajectories generated from the environment.
+                (list): A list payoffs. Each entry corresponds to one player.
+
+        Note: The trajectories are 3-dimension list. The first dimension is for different players.
+              The second dimension is for different transitions. The third dimension is for the contents of each transiton
+        '''
+        trajectories = [[] for _ in range(self.num_players)]
+        state, player_id = self.reset()
+
+        # Loop to play the game
+        trajectories[player_id].append(state)
+        if not self.is_over():
+            # Agent plays
+            # if not is_training:
+            #     action, _ = self.agents[player_id].eval_step(state)
+            # else:
+            #     action = self.agents[player_id].step(state)
+
+            # Environment steps
+            next_state, next_player_id = self.step(action, True)
+            # Save action
+            trajectories[player_id].append(action)
+
+            # Set the state and player
+            state = next_state
+            player_id = next_player_id
+
+            # Save state.
+            if not self.game.is_over():
+                trajectories[player_id].append(state)
+
+        # Add a final state to all the players
+        for player_id in range(self.num_players):
+            state = self.get_state(player_id)
+            trajectories[player_id].append(state)
+
+        # Payoffs
+        payoffs = self.get_payoffs()
+
+        return trajectories, payoffs
+
     def run(self, is_training=False):
         '''
         Run a complete game, either for evaluation or training RL agent.
@@ -241,6 +292,8 @@ class Env(object):
         '''
         raise NotImplementedError
 
+    def decode_action_api(self, action_id):
+        raise NotImplementedError
     def _decode_action(self, action_id):
         ''' Decode Action id to the action in the game.
 
